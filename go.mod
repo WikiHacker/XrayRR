789df88f8e7f0ea6183b57cf743a211cd980d27a
frainzy1477 <49537471+frainzy1477@users.noreply.github.com>
2021-07-17 13:52:23 +0000

    1

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/XrayR-project/XrayR
+
+go 1.16
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
+	github.com/bitly/go-simplejson v0.5.0
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/deckarep/golang-set v1.7.1
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/go-acme/lego/v4 v4.3.1
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/go-resty/resty/v2 v2.5.0
+	github.com/golang/protobuf v1.5.2
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/juju/ratelimit v1.0.1
+	github.com/r3labs/diff/v2 v2.13.1
+	github.com/shirou/gopsutil v3.21.2+incompatible
+	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.7.0
+	github.com/tklauser/go-sysconf v0.3.4 // indirect
+	github.com/urfave/cli v1.22.5
+	github.com/xtls/xray-core v1.4.2
+	golang.org/x/net v0.0.0-20210330230544-e57232859fb2
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.26.0
+)
---
+++ panel/panel.go
@@ -0,0 +1,177 @@
+package panel
+
+import (
+	"encoding/json"
+	io "io/ioutil"
+	"log"
+	"sync"
+
+	"github.com/XrayR-project/XrayR/api"
+	"github.com/XrayR-project/XrayR/api/pmpanel"
+	"github.com/XrayR-project/XrayR/api/proxypanel"
+	"github.com/XrayR-project/XrayR/api/sspanel"
+	"github.com/XrayR-project/XrayR/api/v2board"
+	"github.com/XrayR-project/XrayR/app/mydispatcher"
+	_ "github.com/XrayR-project/XrayR/main/distro/all"
+	"github.com/XrayR-project/XrayR/service"
+	"github.com/XrayR-project/XrayR/service/controller"
+	"github.com/imdario/mergo"
+	"github.com/r3labs/diff/v2"
+	"github.com/xtls/xray-core/app/proxyman"
+	"github.com/xtls/xray-core/app/stats"
+	"github.com/xtls/xray-core/common/serial"
+	"github.com/xtls/xray-core/core"
+	"github.com/xtls/xray-core/infra/conf"
+)
+
+// Panel Structure
+type Panel struct {
+	access      sync.Mutex
+	panelConfig *Config
+	Server      *core.Instance
+	Service     []service.Service
+	Running     bool
+}
+
+func New(panelConfig *Config) *Panel {
+	p := &Panel{panelConfig: panelConfig}
+	return p
+}
+
+func (p *Panel) loadCore(panelConfig *Config) *core.Instance {
+	// Log Config
+	coreLogConfig := &conf.LogConfig{}
+	logConfig := getDefaultLogConfig()
+	if panelConfig.LogConfig != nil {
+		if _, err := diff.Merge(logConfig, panelConfig.LogConfig, logConfig); err != nil {
+			log.Panicf("Read Log config failed: %s", err)
+		}
+	}
+	coreLogConfig.LogLevel = logConfig.Level
+	coreLogConfig.AccessLog = logConfig.AccessPath
+	coreLogConfig.ErrorLog = logConfig.ErrorPath
+
+	// DNS config
+	coreDnsConfig := &conf.DNSConfig{}
+	if panelConfig.DnsConfigPath != "" {
+		if data, err := io.ReadFile(panelConfig.DnsConfigPath); err != nil {
+			log.Panicf("Failed to read dns.json at: %s", panelConfig.DnsConfigPath)
+		} else {
+			if err = json.Unmarshal(data, coreDnsConfig); err != nil {
+				log.Panicf("Failed to unmarshal dns.json")
+			}
+		}
+	}
+	dConfig, err := coreDnsConfig.Build()
+	if err != nil {
+		log.Panicf("Failed to understand dns.json, Please check: https://xtls.github.io/config/base/dns/ for help: %s", err)
+	}
+	// Policy config
+	levelPolicyConfig := parseConnectionConfig(panelConfig.ConnetionConfig)
+	corePolicyConfig := &conf.PolicyConfig{}
+	corePolicyConfig.Levels = map[uint32]*conf.Policy{0: levelPolicyConfig}
+	pConfig, _ := corePolicyConfig.Build()
+	config := &core.Config{
+		App: []*serial.TypedMessage{
+			serial.ToTypedMessage(coreLogConfig.Build()),
+			serial.ToTypedMessage(&mydispatcher.Config{}),
+			serial.ToTypedMessage(&stats.Config{}),
+			serial.ToTypedMessage(&proxyman.InboundConfig{}),
+			serial.ToTypedMessage(&proxyman.OutboundConfig{}),
+			serial.ToTypedMessage(pConfig),
+			serial.ToTypedMessage(dConfig),
+		},
+	}
+	server, err := core.New(config)
+	if err != nil {
+		log.Panicf("failed to create instance: %s", err)
+	}
+	log.Printf("Xray Core Version: %s", core.Version())
+
+	return server
+}
+
+// Start Start the panel
+func (p *Panel) Start() {
+	p.access.Lock()
+	defer p.access.Unlock()
+	log.Print("Start the panel..")
+	// Load Core
+	server := p.loadCore(p.panelConfig)
+	if err := server.Start(); err != nil {
+		log.Panicf("Failed to start instance: %s", err)
+	}
+	p.Server = server
+	// Load Nodes config
+	for _, nodeConfig := range p.panelConfig.NodesConfig {
+		var apiClient api.API
+		switch nodeConfig.PanelType {
+		case "SSpanel":
+			apiClient = sspanel.New(nodeConfig.ApiConfig)
+		case "V2board":
+			apiClient = v2board.New(nodeConfig.ApiConfig)
+		case "PMpanel":
+			apiClient = pmpanel.New(nodeConfig.ApiConfig)
+		case "Proxypanel":
+			apiClient = proxypanel.New(nodeConfig.ApiConfig)
+		default:
+			log.Panicf("Unsupport panel type: %s", nodeConfig.PanelType)
+		}
+		var controllerService service.Service
+		// Regist controller service
+		controllerConfig := getDefaultControllerConfig()
+		if nodeConfig.ControllerConfig != nil {
+			if err := mergo.Merge(controllerConfig, nodeConfig.ControllerConfig, mergo.WithOverride); err != nil {
+				log.Panicf("Read Controller Config Failed")
+			}
+		}
+		controllerService = controller.New(server, apiClient, controllerConfig)
+		p.Service = append(p.Service, controllerService)
+
+	}
+
+	// Start all the service
+	for _, s := range p.Service {
+		err := s.Start()
+		if err != nil {
+			log.Panicf("Panel Start fialed: %s", err)
+		}
+	}
+	p.Running = true
+	return
+}
+
+// Close Close the panel
+func (p *Panel) Close() {
+	p.access.Lock()
+	defer p.access.Unlock()
+	for _, s := range p.Service {
+		err := s.Close()
+		if err != nil {
+			log.Panicf("Panel Close fialed: %s", err)
+		}
+	}
+	p.Server.Close()
+	p.Running = false
+	return
+}
+
+func parseConnectionConfig(c *ConnetionConfig) (policy *conf.Policy) {
+	connetionConfig := getDefaultConnetionConfig()
+	if c != nil {
+		if _, err := diff.Merge(connetionConfig, c, connetionConfig); err != nil {
+			log.Panicf("Read ConnetionConfig failed: %s", err)
+		}
+	}
+	policy = &conf.Policy{
+		StatsUserUplink:   true,
+		StatsUserDownlink: true,
+		Handshake:         &connetionConfig.Handshake,
+		ConnectionIdle:    &connetionConfig.ConnIdle,
+		UplinkOnly:        &connetionConfig.UplinkOnly,
+		DownlinkOnly:      &connetionConfig.DownlinkOnly,
+		BufferSize:        &connetionConfig.BufferSize,
+	}
+
+	return
+}
